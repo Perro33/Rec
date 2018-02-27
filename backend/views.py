7676694196516a21dcf3df6af6f1276aa522f226
zhengyun <zhengyun@sinorail.com>
2018-02-27 11:10:05 +0800

    add mysql support

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 
 # Create your views here.
+
+@require_http_methods(['GET'])
+def get_result(request):
+    response = {}
+    response['msg'] = "ok"
+
+    return JsonResponse(response)
